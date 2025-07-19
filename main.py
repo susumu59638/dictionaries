0c2f69c9d3179d0229f3a453c5080708b815d350
susumu59638 <susumusaito50@gmail.com>
2025-07-19 17:49:59 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#create a dictionary
+names = {"Son":"Susumu","Mother":"Ayumi","Father":"Satoshi","Daughter":"Akira"}
+print(names)
+
+#access a value from dictionary
+my_name = names["Son"]
+print(f"My name is {my_name}")
+
+#get the list of keys
+my_keys = list(names.keys())
+print(my_keys)
+
+#get the list of values
+my_values = list(names.values())
+print(my_values)
