eaf1309fedebd89fc01cb826109a29a6c8835b87
susumu59638 <susumusaito50@gmail.com>
2025-07-20 14:36:09 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -13,3 +13,38 @@ print(my_keys)
 #get the list of values
 my_values = list(names.values())
 print(my_values)
+
+#access each key from the dictionary
+for key in names:
+    print(key)
+
+#access each value from the dictionary
+for key in names:
+    print(names[key])
+
+#check if a key exists in the dictionary
+if "Grandmother" in names:
+    print(names["Grandmother"])
+else:
+    print("This is not a key in here")
+
+#adding a key-value pair in the dictionary
+names["Grandmother"] = "Toshiko"
+names["Grandfather"] = "Otani"
+print(names)
+
+#deleting a key value pair from the dictionary
+del names["Daughter"]
+print(names)
+
+#changing a value in the dictionary
+names["Son"] = "BOB"
+print(names)
+
+#add a list of values to the dictionary
+names["age"] = [12,46,46,78,82]
+print(names)
+
+#access a value from the list in the dictionary
+my_age = names["age"][0]
+print(f"My age is {my_age}") 
